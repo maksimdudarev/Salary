8aed3079afe61812f48868da6b5b98726f4686ea
maksim dudarev <maksim.dudarev@gmail.com>
2019-07-13 18:01:11 +0300

    input date from console, english names

--- src/Application.cs
+++ src/Application.cs
@@ -16,18 +16,24 @@ namespace MD.Salary.Application
             var employeeList = new List<Employee> { };
             foreach (var employeeDB in employeeListDB) employeeList.Add(new Employee(employeeDB));
             foreach (var employee in employeeList) employee.CalculateSubordinate(employeeList);
-            DateTime salaryDate;
-            salaryDate = DateTime.Today;
-            //salaryDate = DateTime.Parse("17/1/7");
+            DateTime salaryDate = InputData();
             foreach (var employee in employeeList) employee.GetSalary(salaryDate);
             foreach (var employee in employeeList) WriteSalary(employee);
-            Console.WriteLine("Итого = " + Round(SalaryCache.GetSum()));
-            Console.Read();
+            Console.WriteLine("\nTotal = " + Round(SalaryCache.GetSum()) + " Date = " + salaryDate);
+            Console.ReadLine();
+        }
+        public static DateTime InputData()
+        {
+            string inputDate = "17/1/7";
+            Console.WriteLine("Input date (for ex., " + inputDate + "): ");
+            inputDate = Console.ReadLine();
+            if (!DateTime.TryParse(inputDate, out DateTime salaryDate)) salaryDate = DateTime.Today;
+            return salaryDate;
         }
         public static void WriteSalary(Employee employee)
         {
             Console.WriteLine(employee.ID + " " + employee.Name + " " + employee.Group + " " +
-                employee.HireDate.ToString("dd MMMM yyyy") + " зп = " + Round(SalaryCache.GetValue(employee.ID)));
+                employee.HireDate.ToString("dd MMMM yyyy") + " salary = " + Round(SalaryCache.GetValue(employee.ID)));
         }
         public static MemoizationCache SalaryCache = new MemoizationCache();
     }
