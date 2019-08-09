94497a8a7e52537a07a6b4ab28e17ffb7853571c
maksim.dudarev <maksim.dudarev@gmail.com>
2019-08-09 12:26:49 +0300

    for running

--- test/TestingControllersSample/src/TestingControllersSample/Program.cs
+++ test/TestingControllersSample/src/TestingControllersSample/Program.cs
@@ -13,6 +13,8 @@ namespace TestingControllersSample
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseDefaultServiceProvider(options =>
+                    options.ValidateScopes = false);
     }
 }
