f3afe7ae9cdbf06c2a625683c83d10a44503c573
maksim.dudarev <maksim.dudarev@gmail.com>
2019-09-27 16:05:03 +0300

    add PBKDF2 example

---
+++ tutorial/hashing-passwords-md5-bcrypt-pbkdf2-csharp-dotnetcore/SimonGilbert.Blog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimonGilbert.Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = "simongilbert123";
+
+            var md5Hash = CryptographyService.HashPasswordUsingMD5(password);
+
+            Console.WriteLine("MD5 Hash:");
+            Console.WriteLine(md5Hash);
+
+            var pbkdf2Hash = CryptographyService.HashPasswordUsingPBKDF2(password);
+
+            Console.WriteLine("PBKDF2 Hash:");
+            Console.WriteLine(pbkdf2Hash);
+
+            Console.ReadKey();
+        }
+    }
+}
