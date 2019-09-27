79613a8c8e5d92927ef1dd36f14e8fb51be50fa4
maksim.dudarev <maksim.dudarev@gmail.com>
2019-09-27 12:39:24 +0300

    api add authentication controller

---
+++ api/Controllers/AuthenticationController.cs
@@ -0,0 +1,15 @@
+﻿using MD.Salary.WebApi.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MD.Salary.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public void Authentication(User user)
+        {
+
+        }
+    }
+}
