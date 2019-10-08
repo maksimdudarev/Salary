6ac4ae639146c7c288d7d696d94d018a4f9c5256
maksim.dudarev <maksim.dudarev@gmail.com>
2019-10-08 16:52:20 +0300

    contactsapi add to solution

--- tutorial/ContactsApi/Startup.cs
+++ tutorial/ContactsApi/Startup.cs
@@ -29,7 +29,7 @@ namespace ContactsApi
 
 
             //using Dependency Injection
-            services.AddSingleton<IContactsRepository, ContactsRepository>();            
+            services.AddScoped<IContactsRepository, ContactsRepository>();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
