1abf842fdbb3c2d63a4b3814a2a43951c5c3f274
maksim.dudarev <maksim.dudarev@gmail.com>
2019-10-08 17:06:14 +0300

    api add contactsapi files

--- api/Middleware/Contacts.cs
+++ api/Middleware/Contacts.cs
@@ -1,9 +1,21 @@
-﻿namespace ContactsApi.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApi.Models
 {
     public class Contacts
     {
-        public string MobilePhone { get; internal set; }
-        public string FirstName { get; internal set; }
-        public string LastName { get; internal set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsFamilyMember { get; set; }
+        public string Company { get; set; }
+        public string JobTitle { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime AnniversaryDate { get; set; }
     }
 }
--- api/Middleware/ContactsRepository.cs
+++ api/Middleware/ContactsRepository.cs
@@ -1,24 +1,39 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ContactsApi.Models;
+using ContactsApi.Contexts;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactsApi.Repository
 {
     public class ContactsRepository : IContactsRepository
     {
-        static List<Contacts> ContactsList = new List<Contacts>();
-
-        public void Add(Contacts item)
+        ContactsContext _context;
+        public ContactsRepository(ContactsContext context)
         {
-            ContactsList.Add(item);
+            _context = context;
+        }       
+
+        public async Task Add(Contacts item)
+        {            
+            await _context.Contacts.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Contacts>> GetAll()
+        {
+            return await _context.Contacts.ToListAsync();
         }
 
         public bool CheckValidUserKey(string reqkey)
         {
-            var userkeyList = new List<string>();
-            userkeyList.Add("28236d8ec201df516d0f6472d516d72d");
-            userkeyList.Add("38236d8ec201df516d0f6472d516d72c");
-            userkeyList.Add("48236d8ec201df516d0f6472d516d72b");
+            var userkeyList = new List<string>
+            {
+                "28236d8ec201df516d0f6472d516d72d",
+                "38236d8ec201df516d0f6472d516d72c",
+                "48236d8ec201df516d0f6472d516d72b"
+            };
 
             if (userkeyList.Contains(reqkey))
             {
@@ -30,34 +45,38 @@ namespace ContactsApi.Repository
             }
         }
 
-        public Contacts Find(string key)
+        public async Task<Contacts> Find(string key)
         {
-            return ContactsList
+            return await _context.Contacts
                 .Where(e => e.MobilePhone.Equals(key))
-                .SingleOrDefault();
-        }
+                .SingleOrDefaultAsync();
+        }       
 
-        public IEnumerable<Contacts> GetAll()
+        public async Task Remove(string Id)
         {
-            ContactsList.Add(new Contacts() { FirstName = "Mithun", MobilePhone = "2345" });
-            return ContactsList;
-        }
-
-        public void Remove(string Id)
-        {
-            var itemToRemove = ContactsList.SingleOrDefault(r => r.MobilePhone == Id);
+            var itemToRemove = await _context.Contacts.SingleOrDefaultAsync(r => r.MobilePhone == Id);
             if (itemToRemove != null)
-                ContactsList.Remove(itemToRemove);
+            {
+                _context.Contacts.Remove(itemToRemove);
+                await _context.SaveChangesAsync();
+            }
         }
 
-        public void Update(Contacts item)
-        {
-            var itemToUpdate = ContactsList.SingleOrDefault(r => r.MobilePhone == item.MobilePhone);
+        public async Task Update(Contacts item)
+        {            
+            var itemToUpdate = await _context.Contacts.SingleOrDefaultAsync(r => r.MobilePhone == item.MobilePhone);
             if (itemToUpdate != null)
             {
                 itemToUpdate.FirstName = item.FirstName;
                 itemToUpdate.LastName = item.LastName;
+                itemToUpdate.IsFamilyMember = item.IsFamilyMember;
+                itemToUpdate.Company = item.Company;
+                itemToUpdate.JobTitle = item.JobTitle;
+                itemToUpdate.Email = item.Email;
                 itemToUpdate.MobilePhone = item.MobilePhone;
+                itemToUpdate.DateOfBirth = item.DateOfBirth;
+                itemToUpdate.AnniversaryDate = item.AnniversaryDate;
+                await _context.SaveChangesAsync();
             }
         }
     }
