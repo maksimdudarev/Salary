02ec5f7ebea575f8018c00925b465b026cdfd075
maksim.dudarev <maksim.dudarev@gmail.com>
2019-10-31 16:31:02 +0300

    abstract & implementes classes

---
+++ api/Calculation/Models/Entities/EmployeeManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using MD.Salary.WebApi.Core.Models;
+using MD.Salary.WebApi.Utilities;
+
+namespace MD.Salary.WebApi.Models
+{
+    public class EmployeeManager : EmployeeBase
+    {
+        public List<EmployeeFull> SubordinateList { get; set; }
+        public ICalculatorSub SalarySub { get; set; }
+        public EmployeeManager(Employee employeeDB) : base(employeeDB)
+        {
+        }
+        public decimal GetSalary(DateTime salaryDate, MemoizationCache salaryCache)
+        {
+            decimal salary = GetSalaryPersonal(salaryDate) + GetSalarySub(salaryDate, salaryCache);
+            salaryCache.Add(ID, salary);
+            return salary;
+        }
+        private decimal GetSalarySub(DateTime salaryDate, MemoizationCache salaryCache)
+        {
+            decimal salary = SalarySub.GetSalary(SubordinateList, salaryDate, salaryCache);
+            return salary;
+        }
+    }
+}
---
+++ api/Calculation/Models/Entities/EmployeeSimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MD.Salary.WebApi.Core.Models;
+using MD.Salary.WebApi.Utilities;
+
+namespace MD.Salary.WebApi.Models
+{
+    public class EmployeeSimple : EmployeeBase
+    {
+        public EmployeeSimple(Employee employeeDB) : base(employeeDB)
+        {
+        }
+        public decimal GetSalary(DateTime salaryDate, MemoizationCache salaryCache)
+        {
+            decimal salary = GetSalaryPersonal(salaryDate);
+            salaryCache.Add(ID, salary);
+            return salary;
+        }
+    }
+}
